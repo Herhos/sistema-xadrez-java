fbeeb03d8446266c03346535294562f87a24489b
Herhos <herhos@outlook.com>
2020-02-09 14:14:39 -0300

    Primeira classe - Posicao

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,9 +1,13 @@
 package aplicacao;
 
+import tabuleirodejogo.Posicao;
+
 public class Programa {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
 
 	}
 
--- src/tabuleirodejogo/Posicao.java
+++ src/tabuleirodejogo/Posicao.java
@@ -1,5 +1,42 @@
 package tabuleirodejogo;
 
-public class Posicao {
+public class Posicao
+{
+	private int linha;
+	private int coluna;
+	
+	public Posicao(int rown, int column)
+	{
+		super();
+		this.linha = rown;
+		this.coluna = column;
+	}
 
+	public int getLinha()
+	{
+		return linha;
+	}
+
+	public void setLinha(int linha)
+	{
+		this.linha = linha;
+	}
+
+	public int getColuna()
+	{
+		return coluna;
+	}
+
+	public void setColuna(int coluna)
+	{
+		this.coluna = coluna;
+	}
+
+	@Override
+	public String toString()
+	{
+		return linha + ", " + coluna;
+	}
+	
+	
 }
